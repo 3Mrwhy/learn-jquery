83691bae5138037830044aad4ac2bce3153bbfd3
mr why <3mrwhy@gmail.com>
2021-05-23 13:31:36 +0530

    mouse events

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 $(document).ready(function(){
 
-	$("li:gt(1)").css("color", "red");
+	// single click
+	$(".container").click(function(){
+		$(".container").css("background", "lightgrey");
+	});
+
+	// double click
+	$(".container").dblclick(function(){
+		$(".container").css("background", "orange");
+	});
+
+	// right click
+	$(".container").contextmenu(function(){
+		$(".container").css("background", "lightcoral");
+	});
+
+	// mouse enter
+	$(".container").mouseenter(function(){
+		$(".container").css("background", "tan");
+	});
+
+	// mouse leave
+	$(".container").mouseleave(function(){
+		$(".container").css("background", "purple");
+	});
 
 });
