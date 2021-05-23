9b3bcbd488e8cfb5b8bed1d73c442b525ae87be7
mr why <3mrwhy@gmail.com>
2021-05-23 13:32:04 +0530

    structure for practice

--- index.php
+++ index.php
@@ -8,13 +8,14 @@
 
 	<h1>Heading 1</h1>
 	<div class="container" id="box">
-		<h2>Main container</h2>
+		<h2>click to change the color</h2>
 		<p>Do exercitation est velit in est proident adipisicing ea sed in nostrud deserunt qui aute tempor in anim ea.</p>
 	</div>
 	<ul>
 		<li>banana</li>
 		<li id="orange">orange</li>
-		<li id="mango">mango</li>
+		<li id="mango
+		">mango</li>
 		<li>apple</li>
 		<li>pine-apple</li>
 	</ul>
