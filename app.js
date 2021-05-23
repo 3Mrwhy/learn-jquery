28505b39ca288b55a0b87ef4d5f26438d96a52f7
mr why <3mrwhy@gmail.com>
2021-05-23 14:34:42 +0530

    windows events

--- app.js
+++ app.js
@@ -1,31 +1,15 @@
 $(document).ready(function(){
 
-	// form events
+	// window events
 	
-	// focus event
-	$("#sname, #sclass, #scountry").focus(function(){
-		$(this).css("background", "green");
+	// scroll events
+	$(".container").scroll(function(){
+		console.log("you are scrolling");
 	});
 
-	// blur event
-	$("#sname, #sclass, #scountry").blur(function(){
-		$(this).css("background", "");
-	});
-
-	// change event
-	$("#scountry").change(function(){
-		let a = $(this).val();
-		$(".test").html(a);
-	});
-
-	// select event
-	$("#sname, #sclass").select(function(){
-		$(this).css("background", "yellow");
-	});
-
-	// Submit event
-	$("#sform").submit(function(){
-		alert("form submit");
+	// Resize
+	$(window).resize(function(){
+		console.log("you are scrolling");
 	});
 
 });
