674b0e6e9cd1ef7c791ef4fe6eaab738f8476982
mr why <3mrwhy@gmail.com>
2021-05-23 13:10:18 +0530

    adding some dummy test for styling

--- index.php
+++ index.php
@@ -2,16 +2,23 @@
 <html>
 <head>
 	<title>Jquery</title>
+	<link rel="stylesheet" type="text/css" href="./style.css">
 </head>
 <body>
-<div id="container1">
+
 	<h1>Heading 1</h1>
-	<p>Nulla velit dolor incididunt cillum dolor nisi esse ea adipisicing dolore commodo pariatur amet anim velit incididunt officia ut.</p>
-</div>
-<div id="container2">
-	<h1>Heading 2</h1>
-	<p>In consequat pariatur id laborum minim occaecat adipisicing irure laboris elit incididunt exercitation.</p>
-</div>
+	<div class="container">
+		<h2>Main container</h2>
+		<p>Do exercitation est velit in est proident adipisicing ea sed in nostrud deserunt qui aute tempor in anim ea.</p>
+	</div>
+	<ul>
+		<li>banana</li>
+		<li>orange</li>
+		<li>mango</li>
+		<li>apple</li>
+		<li>pine-apple</li>
+	</ul>
+
 <script src="./jQuery.min.js"> </script>
 <script src="./app.js"> </script>
 </body>
