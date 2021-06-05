f78044db720e4e39aa7cbd726fb22048ab39d329
mr why <3mrwhy@gmail.com>
2021-06-05 14:09:54 +0530

    each method

--- app.js
+++ app.js
@@ -1,57 +1,16 @@
 $(document).ready(function(){
 
-	//  Has and is method
+	//  each method
 
 
-	/* @name: 		has method
-	  @defination: 	it is used to target the descendant element of the tag
+	/* @name: 		each 
+	  @defination: 	it it used to loop the all given element
 	*/ 
-	$("p").has('span').css("background", "red");
-
-
-	/* @name: 		is method
-	  @defination: 	it is used to find the particular class, tag availabel of not
-	*/ 
-	$("p").click(function(){
-		if($(this).parent().is('div')){
-			console.log($(this).parent().is('div'));
-		}
+	$("li").each(function(){
+		console.log($(this).text("hello"));
 	});
-	// if($('p').parent().is('div')) {
-	// 	console.log($('p').parent().is('div'));
-	// } else {
-	// 	console.log("not find");
-	// }
 
-
-	/* @name: 		eq method
-	  @defination: 	it is used to target the equation element
-	*/ 
-	// $("p").eq(0).css("background", "red");
-
-
-	/* @name: 		Filter method
-	  @defination: 	it is used to target the filterted element of the parameter
-	*/ 
-	// $("p").filter(".first-container").css("background", "red");
 	
-
-	/* @name: 		slice method
-	  @defination: 	it is used to target the sliced element
-	*/ 
-	// $("ul li").slice(1).css("background", "red");
-
-
-	/* @name: 		nextUntil method
-	  @defination: 	it is used to target the range of next elements
-	*/ 
-	// $("#b").nextUntil("#d").css("background", "red");
-
-
-	/* @name: 		prevUntil method
-	  @defination: 	it is used to target the range of prev elements
-	*/ 
-	// $("#d").prevUntil("#a").css("background", "red");
 
 
 });
